134ea6abfb57f8f4c21d5860b69ca4a6f7eb04c4
nutthaphon <nutthaphon@gmail.com>
2018-04-04 23:39:24 +0700

    Include zip file as  monitored file in apps folder

--- script/mule.sh
+++ script/mule.sh
@@ -11,24 +11,24 @@ bin/mule $1
 Start_Progression() {
 
 	cd $MULE_APPS_HOME
-	no_proc_all=`ls *-anchor.txt 2>/dev/null | wc -l`
+	no_proc_all=`ls *{-anchor.txt,.zip} 2>/dev/null | wc -l`
 	no_proc_started=$no_proc_all
 	
 	echo $no_proc_started apps are there.
 	while  [ $no_proc_started -gt 0 ]; do
-		no_proc_started=`ls *-anchor.txt 2>/dev/null | wc -l`
+		no_proc_started=`ls *{-anchor.txt,.zip} 2>/dev/null | wc -l`
 	done
 	
-	echo "Deploying apps..."
+	echo "Deploying domains and apps ..."
 	
 	while  [ $no_proc -lt $no_proc_all ]; do
 		
 		if [ $no_wait -eq 12 ]; then
-			echo "Time out!"; 
+			echo "There are something wrong, please check mule_ee.log file."; 
 			break; 
 		fi	
 		
-		no_proc=`ls *-anchor.txt 2>/dev/null | wc -l`
+		no_proc=`ls *{-anchor.txt,.zip} 2>/dev/null | wc -l`
 		
 		echo Debug Info: no_prev=$no_prev, no_wait=$no_wait, no_proc_all=$no_proc_all, no_proc=$no_proc, no_diff=$no_diff
 		echo "----------------------------------------------------"
@@ -40,7 +40,7 @@ Start_Progression() {
 			no_prev=$no_proc
 			no_wait=0		
 		elif [ $no_proc -eq 0 ]; then
-			echo "Wait for first apps deploy..."
+			echo "Waiting to deploy first apps ..."
 		else		
 			echo "Deploying..."
 			no_wait=`expr $no_wait + 1`							
